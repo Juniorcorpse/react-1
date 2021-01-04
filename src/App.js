efc8925160531a977cb16e34d7333adf8964e30f
Juniorcorpse <abbathdeath@gmail.com>
2021-01-04 19:59:07 -0300

    Trocando dados entre Componentes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import SearchBox from './components/SearchBox';
 import styled from 'styled-components';
+
 
 
 function App() {
 
+  const [searchText, setSeachText] = useState('');
+  function handleSearchInput(novoTexto){
+    setSeachText(novoTexto);
+  }
   
  
   return(
     <>
       <h1>Lista de tarefas</h1>
-      
+      <SearchBox 
+        frasePadrao="email..."
+        onChangeText={handleSearchInput}
+      />  
+      <SearchBox 
+        frasePadrao={searchText}
+       
+      />     
+      <hr/>
+      Texto procurado {searchText}
     </>
   );
 }
