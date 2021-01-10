343852c4f8eff2174417267317d7c0468e9b4f1e
Juniorcorpse <abbathdeath@gmail.com>
2021-01-10 18:39:59 -0300

    Router Basic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
 
 
 function App() {
   
   return(
-    <>     
-     
-    </>
+    <BrowserRouter>
+      <header>
+        <h1>Meu site</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" >HOME</Link>
+            </li>
+            <li>
+              <Link to="/sobre" >SOBRE</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <hr/>
+      <Switch>
+        <Route exact path="/">
+         <Home />
+        </Route>
+        <Route path="/sobre">
+         <Sobre />
+        </Route>
+      </Switch>
+      <hr/>
+      <footer>
+todos os direitos reservados...
+      </footer>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Sobre.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Sobre(){
+    return(
+        <div>
+    <h4>SOBRE</h4>
+        </div>
+    );
+}
+export default Sobre;
